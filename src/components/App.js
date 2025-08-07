7d716e2ef3d3518dc88945e5035b51eb55851c3b
Neelam123K <153060910+Neelam123K@users.noreply.github.com>
2025-08-07 17:58:05 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
-import React from "react";
+import React, {useState} from "react";
+import LoginForm from './LoginForm.js'
 import './../styles/App.css';
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
   return (
     <div>
         {/* Do not remove the main div */}
+      <h1>Parent component</h1>
+      {isLoggedIn ? (
+        <p>you are logged in !</p>
+      ): (
+        <LoginForm onLogin={handleLogin} />
+      )}
     </div>
   )
 }
---
+++ src/components/LoginForm.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const LoginForm = ({onLogin}) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onLogin(); 
+  }
+  return (
+    <div>
+      <forn onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)} required
+        />
+        <button type="submit">Login</button>
+      </forn>
+    </div>
+  );
+}
+
+export default LoginForm;
